13039752906806af600e6256b310f9cf67bf9a39
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2025-01-21 21:03:11 +0530

    Added car y-axis movement animation

--- car.js
+++ car.js
@@ -8,6 +8,15 @@ class Car{
         this.controls = new Controls();
     }
 
+    update(){
+        if(this.controls.forward){
+            this.y -= 1;
+        }
+        if(this.controls.reverse){
+            this.y += 1;
+        }
+    }
+
     draw(ctx){
         ctx.beginPath();
         ctx.rect(
