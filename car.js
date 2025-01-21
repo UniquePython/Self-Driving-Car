9c757a0eadbec8c31bfd8cae45523729ab997b8f
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2025-01-21 21:32:45 +0530

    Fixed issue#1

--- car.js
+++ car.js
@@ -7,6 +7,9 @@ class Car{
 
         this.speed = 0;
         this.acceleration = 0.1;
+        this.maxSpeed = 1.5;
+        this.maxReverseSpeed = -0.75;
+        this.friction = 0.025;
 
         this.controls = new Controls();
     }
@@ -18,6 +21,21 @@ class Car{
         if(this.controls.reverse){
             this.speed -= this.acceleration;
         }
+
+        if(this.speed>this.maxSpeed){
+            this.speed = this.maxSpeed;
+        }
+        if(this.speed<this.maxReverseSpeed){
+            this.speed = this.maxReverseSpeed;
+        }
+
+        if(this.speed>0){
+            this.speed -= this.friction;
+        }
+        if(this.speed<0){
+            this.speed += this.friction;
+        }
+
         this.y -= this.speed;
     }
 
