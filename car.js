0eb9615e665462948281c63f761b396805d20069
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2025-01-28 21:39:32 +0530

    Added turning and refactored car code

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
-class Car{
-    constructor(x, y, width, height){
+class Car {
+    constructor(x, y, width, height) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -11,47 +11,74 @@ class Car{
         this.maxSpeed = 2;
         this.maxReverseSpeed = -1;
         this.friction = 0.05;
+        this.angle = 0;
 
         this.controls = new Controls();
     }
 
-    update(){
-        if(this.controls.forward){
-            this.speed += this.acceleration;
-        }
-        if(this.controls.reverse){
-            this.speed += this.reverseAcceleration;
-        }
+    update() {
+        this.#move();
+    }
 
-        if(this.speed>this.maxSpeed){
-            this.speed = this.maxSpeed;
-        }
-        if(this.speed<this.maxReverseSpeed){
-            this.speed = this.maxReverseSpeed;
-        }
+    #move() {
+        this.#adjustSpeed();
+        this.#applyFriction();
+        this.#adjustPosition();
+        this.#adjustAngle();
+    }
 
-        if(this.speed>0){
+    #adjustSpeed() {
+        if (this.controls.forward) this.speed += this.acceleration;
+        if (this.controls.reverse) this.speed += this.reverseAcceleration;
+
+        this.speed = Math.min(Math.max(this.speed, this.maxReverseSpeed), this.maxSpeed);
+    }
+
+    #applyFriction() {
+        if (this.speed > 0) {
             this.speed -= this.friction;
-        }
-        if(this.speed<0){
+        } else if (this.speed < 0) {
             this.speed += this.friction;
         }
 
-        if(Math.abs(this.speed)<this.friction){
+        if (Math.abs(this.speed) < this.friction) {
             this.speed = 0;
         }
-
-        this.y -= this.speed;
     }
 
-    draw(ctx){
+    #adjustPosition() {
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
+    }
+
+    #adjustAngle() {
+        if (this.speed !== 0) {
+            const flip = this.speed > 0 ? 1 : -1; // 1 if going forward, -1 if going reverse
+            const radius = this.speed > 0 ? 0.03 : 0.02; // 0.03 if going forward, 0.02 if going reverse
+
+            if (this.controls.left) {
+                this.angle += radius * flip;
+            }
+            if (this.controls.right) {
+                this.angle -= radius * flip;
+            }
+        }
+    }
+
+    draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x-this.width/2,
-            this.y-this.height/2,
+            -this.width / 2,
+            -this.height / 2,
             this.width,
             this.height
         );
-        ctx.fill()
+        ctx.fill();
+
+        ctx.restore();
     }
 }
